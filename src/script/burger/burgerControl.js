d7d253d14d00cde2db8c201cefcd1adb9a382909
Роман Хоружий <roman.khoruzhy@gmail.com>
2024-04-26 23:26:40 +0300

    header menu

--- src/script/burger/burgerControl.js
+++ src/script/burger/burgerControl.js
@@ -1,9 +1,11 @@
 export const burgerControll = ({ selectorBtn, selectorNav }) => {
   const burger = document.querySelector(selectorBtn);
   const navigation = document.querySelector(selectorNav);
+  const header = document.querySelector(".header");
 
   burger.addEventListener("click", () => {
-    console.log("click");
+    burger.classList.toggle("burger_active");
     navigation.classList.toggle("navigation_active");
+    header.classList.toggle("header_active");
   });
 };
